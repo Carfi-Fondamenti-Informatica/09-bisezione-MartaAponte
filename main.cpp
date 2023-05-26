976ba4402a3a5669dfd48300d0ab60b1c746481f
MartaAponte <128090552+MartaAponte@users.noreply.github.com>
2023-05-26 20:13:38 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,38 +2,33 @@
 #include <cmath>
 using namespace std;
 
-float f(float y ){
+float f(float y){
     float x=pow(y,2)*cos(y)+1;
     return x;
 }
 
 int main() {
-    float  a=0 , b=0,x,err ;
-    do {
-        cout <<"Inserire estremi"<< endl;
-        cin >> a >> b ;
-    }
-
-    while  (f(a )*f( b)>= 0);
-    do {
-        x = (a + b) / 2;
-        if (f(x) == 0) {
-            cout << x;
+    float a=0, b=0, x, err;
+    do{
+        cout << "inserire estremi\n";
+        cin >> a >> b;
+    } while (f(a)*f(b)>=0);
+    do{
+        x=(a+b)/2;
+        if (f(x)==0){
             break;
         }
-        else {
-             if(f(a) * f(x) < 0){
-                b = x;}
-             else {
-                 a = x;
-             }
+        else if (f(a)*f(x)<0){
+            b=x;
         }
-
-            err = abs((b-a)/2);
-    }
-    while (err>= 1e-6);
-        cout.precision(4);
-        cout<<x;
-
-        return 0;
+        else {
+            a=x;
+        }
+        err=abs((b-a)/2);
+    }while (err >= (1e-6));
+    x=x*10000;
+    float c=int(x);
+    float d=c/10000;
+    cout <<d;
+    return 0;
 }
